6eb7645b95cc2811a8ff1efff9a31a64759b8276
王佳祺 <stefanmao@wangjiaqideMacBook-Pro.local>
2018-04-07 01:24:55 +0800

    add Options

--- DKG-FrontEnd/src/components/Scheme/Scheme.js
+++ DKG-FrontEnd/src/components/Scheme/Scheme.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import {
     Badge,
+    Button,
+    Form,
+    FormGroup,
     Row,
     Col,
     Card,
@@ -9,17 +12,76 @@ import {
     Table,
     Pagination,
     PaginationItem,
-    PaginationLink
-  } from 'reactstrap';
+    PaginationLink,
+    Label,
+    Input
+} from 'reactstrap';
 
 class Scheme extends Component {
+        constructor(props) {
+        super(props);
+        this.state = {
+            // exam_conditions : [],
+         }
+        this.getAllRelations = this.getAllRelations.bind(this);
+        this.getRelationsByConcept = this.getRelationsByConcept.bind(this);
+        }
+
+        // componentWillMount() {
+            
+        // }
+
+       getAllRelations() {
+          
+       }
+
+       getRelationsByConcept() {
+
+    }
+
+
     render() {
         return (
             <div>
             {/* <div>
                 这里是模式管理
             </div> */}
-            {/* <Button>导入文件</Button> */}
+            <Card>
+              <CardHeader>
+                <strong>Options</strong>
+              </CardHeader>
+              <CardBody>
+                  <Form inline>
+                  <FormGroup className="pr-5">
+                      <Button color="primary" onClick={this.getAllRelations}>获取所有关系</Button>{' '}
+                  </FormGroup>              
+                  
+            
+                  <FormGroup className="pr-5">
+                      <Input type="scheme" name="scheme" id="searchScheme" placeholder="概念名" />
+                      <Button color="primary" onClick={this.getRelationsByConcept}>获取关系</Button>
+                  </FormGroup>
+                  
+                  <FormGroup className="pr-5">
+                    {/* <Col md="3">
+                      <Label htmlFor="file-input">添加关系</Label>
+                    </Col> */}
+                    {/* <Col xs="3" md="3"> */}
+                      <Input type="file" id="file-input" name="file-input"/>
+                    {/* </Col> */}
+                  </FormGroup>
+                </Form>
+
+              </CardBody>
+            </Card>
+
+         
+             
+         
+
+
+           
+
             <Col xs="12" lg="12">
             <Card>
               <CardHeader>
@@ -30,8 +92,8 @@ class Scheme extends Component {
                   <thead>
                   <tr>
                     <th>概念</th>
-                    <th>概念</th>
                     <th>关系</th>
+                    <th>概念</th>
                   </tr>
                   </thead>
                   <tbody>
@@ -90,4 +152,4 @@ class Scheme extends Component {
     }
 }
 
-export default Scheme
+export default Scheme;
