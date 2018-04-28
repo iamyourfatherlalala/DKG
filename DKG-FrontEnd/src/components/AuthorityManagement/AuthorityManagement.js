e86a23fa5118ce2339143f7cb670d63a23ce8ed2
stefanmao <1834979802@qq.com>
2018-04-29 02:31:42 +0800

    add role management

--- DKG-FrontEnd/src/components/AuthorityManagement/AuthorityManagement.js
+++ DKG-FrontEnd/src/components/AuthorityManagement/AuthorityManagement.js
@@ -1,77 +1,20 @@
+require('./AuthorityManagement.css');
 import React, {Component} from 'react';
+import App from './app';
 
 class AuthorityManagement extends Component {
     render() {
         return (
-            <div>
-            {/* <Col xs="12" lg="12">
-            <Card>
-              <CardHeader>
-                <i className="fa fa-align-justify"></i> Simple Table
-              </CardHeader>
-              <CardBody>
-                <Table responsive>
-                  <thead>
-                  <tr>
-                    <th>概念</th>
-                    <th>关系</th>
-                    <th>概念</th>
-                  </tr>
-                  </thead>
-                  <tbody>
-                  <tr>
-                    <td>Samppa Nori</td>
-                    <td>2012/01/01</td>
-                    <td>Member</td>
-                  </tr>
-                  <tr>
-                    <td>Estavan Lykos</td>
-                    <td>2012/02/01</td>
-                    <td>Staff</td>
-                  </tr>
-                  <tr>
-                    <td>Chetan Mohamed</td>
-                    <td>2012/02/01</td>
-                    <td>Admin</td>
-                  </tr>
-                  <tr>
-                    <td>Derick Maximinus</td>
-                    <td>2012/03/01</td>
-                    <td>Member</td>
-                  </tr>
-                  <tr>
-                    <td>Friderik Dávid</td>
-                    <td>2012/01/21</td>
-                    <td>Staff</td>
-                  </tr>
-                  </tbody>
-                </Table>
-                <Pagination>
-                  <PaginationItem>
-                    <PaginationLink previous href="#"></PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem active>
-                    <PaginationLink href="#">1</PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem>
-                    <PaginationLink href="#">2</PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem>
-                    <PaginationLink href="#">3</PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem>
-                    <PaginationLink href="#">4</PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem>
-                    <PaginationLink next href="#"></PaginationLink>
-                  </PaginationItem>
-                </Pagination>
-              </CardBody>
-            </Card>
-          </Col> */}
-          <h1>lalallaalallalaal</h1>
+          <div className='col-md-offset-1 col-md-8'>
+            <div className='panel panel-default'>
+              {/* <div className='panel-heading'>A Complex Example</div> */}
+              <h5>用户管理</h5>
+              <div className='panel-body'>
+                <App />
+              </div>
             </div>
-        )
+          </div>
+        );
     }
 }
 
